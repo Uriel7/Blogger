07ed4a2389eed80fc5c205c5c158d0d9c93296a2
Uriel <j.urielsz@gmail.com>
2017-07-14 22:20:21 -0500

    CRUD to Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,43 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show]
+  before_action :set_article, only: [:show, :destroy, :edit, :update]
   def index
     @articles = Article.all
   end
 
   def new
+    @article = Article.new
   end
 
   def show
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to article_path(@article)
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @article.update(article_params)
+    flash.notice = "Article #{@article.title} updated!"
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    redirect_to articles_path if @article.destroy
   end
 
   private
 
   def set_article
     @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end
