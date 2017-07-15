bf361a6ae01d967b261d63ef481cabada1b3f4b5
Uriel <j.urielsz@gmail.com>
2017-07-14 21:31:28 -0500

    Add style, Articles list, Articles Controller

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,18 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show]
+  def index
+    @articles = Article.all
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+end
